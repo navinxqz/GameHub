299a1032e72ebd39e4e91933c6efae92d40ce5a9
navin-nawshin <navinmdnawshin@gmail.com>
2024-09-25 21:14:09 +0600

    Drop backstyle changed

--- Forms/AddGames.Designer.cs
+++ Forms/AddGames.Designer.cs
@@ -295,8 +295,8 @@
             // 
             this.cbCat.CornerRoundingRadius = 30F;
             this.cbCat.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cbCat.DropBackStyle = Krypton.Toolkit.PaletteBackStyle.ButtonLowProfile;
-            this.cbCat.DropButtonStyle = Krypton.Toolkit.ButtonStyle.LowProfile;
+            this.cbCat.DropBackStyle = Krypton.Toolkit.PaletteBackStyle.ButtonNavigatorMini;
+            this.cbCat.DropButtonStyle = Krypton.Toolkit.ButtonStyle.NavigatorMini;
             this.cbCat.DropDownWidth = 317;
             this.cbCat.IntegralHeight = false;
             this.cbCat.Location = new System.Drawing.Point(43, 369);
