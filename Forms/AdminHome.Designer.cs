0010afde7f20adf944f2d41f980d734bbaa592b6
navin-nawshin <navinmdnawshin@gmail.com>
2024-10-21 20:57:30 +0600

    AdminHome part base

--- Forms/AdminHome.Designer.cs
+++ Forms/AdminHome.Designer.cs
@@ -29,43 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminHome));
-            this.label1 = new System.Windows.Forms.Label();
             this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
-            this.cuiBorder1 = new CuoreUI.Controls.cuiBorder();
-            this.cuiBorder1.SuspendLayout();
+            this.gamePanel = new CuoreUI.Controls.cuiBorder();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(246)))), ((int)(((byte)(224)))));
-            this.label1.Location = new System.Drawing.Point(273, 154);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(321, 42);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Admin DashBoard";
             // 
             // bunifuElipse1
             // 
             this.bunifuElipse1.ElipseRadius = 50;
             this.bunifuElipse1.TargetControl = this;
             // 
-            // cuiBorder1
+            // gamePanel
             // 
-            this.cuiBorder1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("cuiBorder1.BackgroundImage")));
-            this.cuiBorder1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.cuiBorder1.Controls.Add(this.label1);
-            this.cuiBorder1.Location = new System.Drawing.Point(28, 12);
-            this.cuiBorder1.Name = "cuiBorder1";
-            this.cuiBorder1.OutlineThickness = 1F;
-            this.cuiBorder1.PanelColor = System.Drawing.Color.Transparent;
-            this.cuiBorder1.PanelOutlineColor = System.Drawing.Color.Transparent;
-            this.cuiBorder1.Rounding = new System.Windows.Forms.Padding(8);
-            this.cuiBorder1.Size = new System.Drawing.Size(674, 282);
-            this.cuiBorder1.TabIndex = 3;
+            this.gamePanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gamePanel.AutoScroll = true;
+            this.gamePanel.Location = new System.Drawing.Point(29, 68);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.OutlineThickness = 1F;
+            this.gamePanel.PanelColor = System.Drawing.Color.Transparent;
+            this.gamePanel.PanelOutlineColor = System.Drawing.Color.Coral;
+            this.gamePanel.Rounding = new System.Windows.Forms.Padding(8);
+            this.gamePanel.Size = new System.Drawing.Size(1229, 215);
+            this.gamePanel.TabIndex = 0;
             // 
             // AdminHome
             // 
@@ -73,20 +58,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(41)))));
             this.ClientSize = new System.Drawing.Size(1286, 760);
-            this.Controls.Add(this.cuiBorder1);
+            this.Controls.Add(this.gamePanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AdminHome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AdminHome";
-            this.cuiBorder1.ResumeLayout(false);
-            this.cuiBorder1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label1;
         private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
-        private CuoreUI.Controls.cuiBorder cuiBorder1;
+        private CuoreUI.Controls.cuiBorder gamePanel;
     }
 }
--- Forms/AdminHome.cs
+++ Forms/AdminHome.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using GameServer_Management.Class;
+using GameServer_Management.Controller;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +18,64 @@ namespace GameServer_Management.Forms
         public AdminHome()
         {
             InitializeComponent();
+        }
+        //async LoadItems function
+        private async void LoadItems()
+        {
+            string query = "select * from gamestbl t1 inner join categorytbl t2 on t2.catID = t1.categoryID";
+            using (SqlConnection con = DBconnect.GetConnection())
+            {
+                if (con == null)
+                {
+                    MessageBox.Show("Database connection is not established.", "GameHub", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                try
+                {
+                    await con.OpenAsync();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (await reader.ReadAsync())
+                                {
+                                    ListViewItem item = new ListViewItem(reader["gameID"].ToString());
+                                    item.SubItems.Add(reader["gameName"].ToString());
+                                    item.SubItems.Add(reader["gamePrice"].ToString());
+                                    item.SubItems.Add(reader["gameDescription"].ToString());
+                                    item.SubItems.Add(reader["gameImage"].ToString());
+                                    item.SubItems.Add(reader["catName"].ToString());
+                                    //listView1.Items.Add(item);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "GameHub", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void AddItems(string id, string name, string cat, string price, Image img, string gameDescription)
+        {
+            var v = new GameDesc
+            {
+                GName = name,
+                Price = price,
+                Category = cat,
+                Pic = img,
+                id = Convert.ToInt32(id),
+                desc = gameDescription,
+            };
+
+            //v.onSelect += new EventHandler(Game_Click);
+            gamePanel.Controls.Add(v);
+            v.BringToFront();   //loading data backward
+            //gameDescList.Add(v);
         }
     }
 }
